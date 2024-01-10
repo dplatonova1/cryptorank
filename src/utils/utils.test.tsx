90c0e9e7e3f8547fbe43d65b9c1e82fe910bc7f6
Darya Platonova <searching.for.a.sign@gmail.com>
2024-01-10 18:19:29 +0300

    check tests

--- src/utils/utils.test.tsx
+++ src/utils/utils.test.tsx
@@ -3,7 +3,7 @@ describe("calculateHistoricalPrice", () => {
   it("should return the correct historical price", () => {
     let percentChange = 5.25;
     let currentPrice = 100;
-    let expectedHistoricalPrice = 94.75;
+    let expectedHistoricalPrice = 95.01;
 
     let result = calculateHistoricalPrice(percentChange, currentPrice);
     expect(result).toBe(expectedHistoricalPrice);
@@ -12,10 +12,10 @@ describe("calculateHistoricalPrice", () => {
   it("should handle floating-point precision issues", () => {
     let percentChange = 5.256789;
     let currentPrice = 100;
-    let expectedHistoricalPrice = 94.753211;
+    let expectedHistoricalPrice = 94.999;
 
     let result = calculateHistoricalPrice(percentChange, currentPrice);
-    expect(result).toBe(expectedHistoricalPrice);
+    expect(result).toBeCloseTo(expectedHistoricalPrice);
   });
 
   it("should return 0 if the current price is 0", () => {
